71f7453ded488da1ce5890228d1d95eb6ed22fed
LucasPapini <papine12345@hotmail.com>
2023-10-31 21:42:54 -0300

    add atualizacao contexto-formulario

--- 02-ignite-time/src/page/Home/components/NewCycleForm/index.tsx
+++ 02-ignite-time/src/page/Home/components/NewCycleForm/index.tsx
@@ -1,37 +1,43 @@
-/**
- * [STYLES]
- */
-import * as S from './styles'
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
+import { useFormContext } from 'react-hook-form'
 
 export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
   return (
-    <S.FormContainer>
+    <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
-      <S.TaskInput
+      <TaskInput
         id="task"
-        type="text"
+        list="task-suggestions"
         placeholder="Dê um nome para o seu projeto"
-        list="taks-suggestions"
-        {...register("task")}
         disabled={!!activeCycle}
+        {...register('task')}
       />
 
       <datalist id="task-suggestions">
-        <option value="Projeto 1"></option>
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Banana" />
       </datalist>
 
-      <label htmlFor="">durante</label>
-      <S.MinutesAmountInput
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
         type="number"
         id="minutesAmount"
         placeholder="00"
         step={5}
-        min={1}
+        min={5}
         max={60}
         disabled={!!activeCycle}
-        {...register("minutesamount", { valueAsNumber: true })}
+        {...register('minutesAmount', { valueAsNumber: true })}
       />
+
       <span>minutos.</span>
-    </S.FormContainer>
+    </FormContainer>
   )
 }
